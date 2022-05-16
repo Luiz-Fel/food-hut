e9f8b535893fe5c36f643ffcf8eb298a12a0a04f
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2022-05-16 13:19:30 -0300

    Added NavBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,50 @@
+import Image from 'next/image'
 import React from 'react'
+import styles from './styles.module.scss'
+import call from '../../../public/call.svg'
+import cart from '../../../public/cart.svg'
+
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <a href="">
+          <h1>Food<span>-</span>Hut</h1>
+        </a>
+      </div>
+      <div className={styles.item}>
+        <a href="">
+          HOME
+        </a>
+      </div>
+      <div className={styles.item}>
+        <a href="">
+          ORDER NOW
+        </a>
+      </div>
+      <div className={styles.item}>
+        <a href="">
+          MENU
+        </a>
+      </div>
+      <div className={styles.item}>
+        <a href="">
+          CONTACT
+        </a>
+      </div>
+      <div className={styles.contactAndCart}>
+
+      <div className={styles.callButton}>
+          <Image src={call} />
+            <div className={styles.text}>
+              +1-650-546-9864
+            </div>
+      </div>
+      <div className={styles.cart}>
+          <Image src={cart}  width={23}/>
+      </div>
+      </div>
+    </div>
   )
 }
