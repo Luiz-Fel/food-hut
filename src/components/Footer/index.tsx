2c1ac7e759420b3f6177f5bb1fff32f2a8efd1b2
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2022-05-18 11:36:09 -0300

    Added footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,67 @@
+import Image from 'next/image'
 import React from 'react'
+import styles from './styles.module.scss'
+import restaurant from '../../../public/images/restaurant.jpg'
+import facebook from '../../../public/facebook.svg'
+import instagram from '../../../public/instagram.svg'
+import twitter from '../../../public/twitter.svg'
+import youtube from '../../../public/youtube.svg'
+
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <div className={styles.container}>
+      <Image src={restaurant}  width={300} height={400} />
+      <div className={styles.content}>
+        <div className={styles.column}>
+          <h2 className={styles.slogan}>
+            PILLING BURGERS HIGH SINCE 1958.
+          </h2>
+        </div>
+        <div className={styles.column}>
+            <h3 className={styles.columnTitle}>
+              FIND OUR RESTAURANTS
+            </h3>
+            <p>1654 R. Don Road #304.
+              <br />NewYork, 85022
+              <br />(602) 867-1010
+            </p>
+            <p>1654 R. Don Road #304.
+              <br />NewYork, 85022
+              <br />(602) 867-1010
+            </p>            
+            <p>1654 R. Don Road #304.
+              <br />NewYork, 85022
+              <br />(602) 867-1010
+            </p>
+        </div>
+        <div className={styles.column}>
+          <h3 className={styles.columnTitle}>
+            WORKING HOURS
+          </h3>
+          <p>MONDAY UNTIL FRIDAY<br />9:00-22:00</p>
+          <p>SATURDAY - SUNDAY<br />12:00-24:00</p>
+        </div>
+        <div className={styles.socials}>
+          <a href="/">
+            <Image src={facebook} />
+          </a>
+          <a href="/">
+            <Image src={instagram} />
+          </a>
+          <a href="/">
+            <Image src={twitter} />
+          </a>
+          <a href="/">
+            <Image src={youtube} />
+          </a>
+        </div>
+      </div>
+    </div>
+      <div className={styles.copyright}>
+
+      </div>
+    </>
   )
 }
