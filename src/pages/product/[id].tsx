de11f3433410f35d81b502f580b2c536429e4bf3
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2022-05-20 11:09:18 -0300

    Updated product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -5,19 +5,29 @@ import styles from './styles.module.scss'
 export default function Product() {
     const burger = {
         id: 0,
-        img: '/images/singlePatty.png',
+        img: 'singlePatty.png',
         name: 'Single Patty',
         price: [7.99, 9.99, 11.99],
-        description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit in culpa'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada eros quis bibendum. Sed sodales a eros vestibulum tincidunt. Integer urna neque, sodales ac varius sit amet, luctus et leo. Donec convallis turpis sed hendrerit euismod. Donec egestas dapibus consequat.'
     };
+
+    const loader = ({ src  } : any) => {
+        return `/images/${src}`
+    }
 
   return (
     <div className={styles.container}>
-        <Image src={"../../../public" + burger.img} />
+        <Image loader={loader} src={burger.img} width={600} height={600}/>
         <div className={styles.content}>
             <h2 className={styles.name}>{burger.name}</h2>
-            <p className={styles.price}>{burger.price[0]}</p>
+            <p className={styles.price}>{
+                new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD'  
+                }).format(burger.price[0])
+            }</p>
             <p className={styles.description}>{burger.description}</p>
+            <h3 className={styles.optionsTitle}>Choose additional ingredients</h3>
             <div className={styles.additional}>
                 <div className={styles.option}>
                     <label>
