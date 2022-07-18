64d89e9c0dedd6e25a547454fe3f0be0e0b3df79
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2022-07-18 20:10:47 -0300

    Updated cart price.

--- src/components/NavBar/Cart/index.tsx
+++ src/components/NavBar/Cart/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React, { Dispatch } from 'react'
 import ReactModal from 'react-modal';
 import styles from './styles.module.scss'
@@ -15,7 +16,21 @@ const products = [{
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra malesuada eros quis bibendum. Sed sodales a eros vestibulum tincidunt. Integer urna neque, sodales ac varius sit amet, luctus et leo. Donec convallis turpis sed hendrerit euismod. Donec egestas dapibus consequat.'
 }]
 
-const totalValue = 7.99;
+const totalValue = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'  
+  }).format(7.99);
+
+const quantity = 1;
+
+const price = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'  
+  }).format(products[0].price[0])
+  
+const loader = ({ src  } : any) => {
+  return `/images/${src}`
+}
 
 export default function Cart({ modalIsOpen, setModalIsOpen } : CartProps) {
   return (
@@ -32,8 +47,15 @@ export default function Cart({ modalIsOpen, setModalIsOpen } : CartProps) {
       <div className={styles.container}>
         {products.map((product) => {
           return (
-            <div>
-              
+            <div className={styles.product}>
+              <Image loader={loader} src={product.img} width={70} height={70}/>
+              <div className={styles.content}>
+                <p>{product.name}</p>
+                <div className={styles.lowerContent}>
+                  <p>{quantity}</p>
+                  <p>{price}</p>
+                </div>
+              </div>
             </div>
           )
         })}
