29026b2b2b49b8e37651d80d7407a9d37a0bba2d
Luiz Felipe <luizfelipesantospereira01@gmail.com>
2022-07-29 13:49:16 -0300

    Added cart buttons

--- src/components/NavBar/Cart/CartProduct/index.tsx
+++ src/components/NavBar/Cart/CartProduct/index.tsx
@@ -7,24 +7,35 @@ interface CartProductProps {
     img: string;
     name: string;
   };
-  quantity: number;
+  extras: number;
   price: string;
 }
 const loader = ({ src } : any) => {
   return `/images/${src}`
 }
 
-export default function CartProduct({ product, quantity, price } : CartProductProps) {
+export default function CartProduct({ product, extras, price } : CartProductProps) {
   return (
-    <div className={styles.product}>
-    <Image loader={loader} src={product.img} width={70} height={70}/>
-    <div className={styles.content}>
-      <p>{product.name}</p>
-      <div className={styles.lowerContent}>
-        <p>{quantity}</p>
+  <div className={styles.product}>
+      <div className={styles.upperContent}>
+        <Image loader={loader} src={product.img} width={70} height={70}/>
+        <p>{product.name}</p>
+      <div className={styles.price}>
         <p>{price}</p>
       </div>
     </div>
+      <div className={styles.lowerContent}>
+        <p>{extras}</p>
+    </div>
+    <div className={styles.buttons}>
+      <button className={styles.editButton}>
+        Edit
+      </button>
+      <button className={styles.removeButton}>
+        Remove
+      </button>
+    </div>
   </div>
+  
   )
 }
